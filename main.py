e983871e15c61d51fb0b490df4e146c34fc66e03
SaraySantos <saraysantoscaballero@gmail.com>
2025-05-24 00:14:31 -0500

    755

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from frontend.screens.practica2_screen import Practica2Screen
 from frontend.screens.practica3_screen import Practica3Screen
 from frontend.screens.practica4_screen import Practica4Screen
 from frontend.screens.practica5_screen import Practica5Screen
+from frontend.screens.practica6_monitor_screen import Practica6MonitorScreen
 
 # Cargar los archivos .kv
 Builder.load_file('kv/main.kv')
@@ -17,6 +18,7 @@ Builder.load_file('kv/practica2.kv')
 Builder.load_file('kv/practica3.kv')
 Builder.load_file('kv/practica4.kv')
 Builder.load_file('kv/practica5.kv')
+Builder.load_file('kv/practica6.kv')
 
 class RootWidget(ScreenManager):
     pass
@@ -30,6 +32,7 @@ class MainApp(App):
         sm.add_widget(Practica3Screen(name='practica3'))
         sm.add_widget(Practica4Screen(name='practica4'))
         sm.add_widget(Practica5Screen(name='practica5'))
+        sm.add_widget(Practica6MonitorScreen(name='practica6_monitor'))
         return sm
 
 # Punto de entrada de la app
